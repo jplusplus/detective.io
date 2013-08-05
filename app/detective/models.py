40b2614e8c2ff8c2be925b1bc63e60537339660f
Pierre Romera <hello@pirhoo.com>
2013-08-05 22:46:46 +0200

    Added default input for multiple values

--- app/detective/models.py
+++ app/detective/models.py
@@ -13,8 +13,8 @@ class Country(models.NodeModel):
 class FundraisingRound(Amount):
     currency = models.StringProperty(null=True)
     raiseType = models.StringProperty(null=True)
-    personalpayer = models.Relationship("Person",rel_type='hasPersonalPayer', null=True, related_name='+')
-    payer = models.Relationship("Organization",rel_type='hasPayer', null=True, related_name='+')
+    personalpayer = models.Relationship("Person",rel_type='hasPersonalPayer', null=True)
+    payer = models.Relationship("Organization",rel_type='hasPayer', null=True)
 
 class Organization(models.NodeModel):
     officeAddress = models.StringProperty(null=True)
@@ -25,12 +25,12 @@ class Organization(models.NodeModel):
     source = models.URLProperty(null=True)
     twitterHandle = models.StringProperty(null=True)
     founded = models.DateTimeProperty(null=True)
-    boardmember = models.Relationship("Person",rel_type='hasBoardMember', null=True, related_name='+')
-    revenue = models.Relationship("Revenue",rel_type='hasRevenue', null=True, related_name='+')
-    adviser = models.Relationship("Person",rel_type='hasAdviser', null=True, related_name='+')
-    fundraisinground = models.Relationship("FundraisingRound",rel_type='hasFundraisingRound', null=True, related_name='+')
-    partner = models.Relationship("self",rel_type='hasPartner', null=True, related_name='+')
-    sponsor = models.Relationship("self",rel_type='hasSponsor', null=True, related_name='+')
+    boardmember = models.Relationship("Person",rel_type='hasBoardMember', null=True)
+    revenue = models.Relationship("Revenue",rel_type='hasRevenue', null=True)
+    adviser = models.Relationship("Person",rel_type='hasAdviser', null=True)
+    fundraisinground = models.Relationship("FundraisingRound",rel_type='hasFundraisingRound', null=True)
+    partner = models.Relationship("self",rel_type='hasPartner', null=True)
+    sponsor = models.Relationship("self",rel_type='hasSponsor', null=True)
 
     def __unicode__(self):
         return self.name
@@ -46,10 +46,10 @@ class Project(models.NodeModel):
     comment = models.StringProperty(null=True)
     ended = models.DateTimeProperty(null=True, blank=True)
     twitterHandle = models.StringProperty(verbose_name="Twitter Handle", null=True)
-    activityin = models.Relationship("Country",rel_type='hasActivityIn', null=True, related_name='+')
-    owner = models.Relationship("Organization",rel_type='hasOwner', null=True, related_name='+')
-    commentary = models.Relationship("Commentary",rel_type='hasCommentary', null=True, related_name='+')
-    partner = models.Relationship("Organization",rel_type='hasPartner', null=True, related_name='+')
+    activityin = models.Relationship("Country",rel_type='hasActivityIn', null=True)
+    owner = models.Relationship("Organization",rel_type='hasOwner', null=True)
+    commentary = models.Relationship("Commentary",rel_type='hasCommentary', null=True)
+    partner = models.Relationship("Organization",rel_type='hasPartner', null=True)
     def __unicode__(self):
         return self.name
 
@@ -57,13 +57,13 @@ class Commentary(models.NodeModel):
     year = models.DateTimeProperty(null=True)
     articleUrl = models.URLProperty(null=True)
     title = models.StringProperty(null=True)
-    author = models.Relationship("Person",rel_type='hasAuthor', null=True, related_name='+')
+    author = models.Relationship("Person",rel_type='hasAuthor', null=True)
 
 class Distribution(Amount):
     sold = models.StringProperty(null=True)
 
 class EnergyProject(Project):
-    product = models.Relationship("EnergyProduct",rel_type='hasProduct', null=True, related_name='+')
+    product = models.Relationship("EnergyProduct",rel_type='hasProduct', null=True)
 
 class InternationalOrganization(Organization):
     pass
@@ -74,9 +74,9 @@ class Person(models.NodeModel):
     websiteUrl = models.StringProperty(null=True)
     twitterHandle = models.StringProperty(null=True)
     image = models.URLProperty(null=True)
-    hasActivityIn = models.Relationship("Organization",rel_type='hasActivityIn', null=True, related_name='+')
-    nationality = models.Relationship("Country",rel_type='hasNationality', null=True, related_name='+')
-    previousactivityin = models.Relationship("Organization",rel_type='hasPreviousActivityIn', null=True, related_name='+')
+    hasActivityIn = models.Relationship("Organization",rel_type='hasActivityIn', null=True)
+    nationality = models.Relationship("Country",rel_type='hasNationality', null=True)
+    previousactivityin = models.Relationship("Organization",rel_type='hasPreviousActivityIn', null=True)
     def __unicode__(self):
         return self.name
 
@@ -94,12 +94,12 @@ class Product(models.NodeModel):
     name = models.StringProperty()
     source = models.URLProperty(null=True)
     image = models.URLProperty(null=True)
-    price = models.Relationship("Price",rel_type='hasPrice', null=True, related_name='+')
+    price = models.Relationship("Price",rel_type='hasPrice', null=True)
 
 class EnergyProduct(Product):
     powerGenerationPerUnitInWatt = models.IntegerProperty(null=True)
-    distribution = models.Relationship("Distribution",rel_type='hasDistribution', null=True, related_name='+')
-    operator = models.Relationship("Organization",rel_type='hasOperator', null=True, related_name='+')
+    distribution = models.Relationship("Distribution",rel_type='hasDistribution', null=True)
+    operator = models.Relationship("Organization",rel_type='hasOperator', null=True)
 
 class Ngo(Organization):
     pass

0b4d43e11e6e715321aa7654ac13a93471b47e70
Pierre Romera <hello@pirhoo.com>
2013-08-22 15:05:47 +0200

    Added middleware to support Cross-domain xhr

---
+++ app/middleware/crossdomainxhr.py
@@ -0,0 +1,46 @@
+# Middleware that allows cross-origin request
+# https://gist.github.com/1369619
+# 
+# More information about cross-origin: http://enable-cors.org/index.html
+from django import http
+
+try:
+    from django.conf import settings
+    XS_SHARING_ALLOWED_ORIGINS = settings.XS_SHARING_ALLOWED_ORIGINS
+    XS_SHARING_ALLOWED_METHODS = settings.XS_SHARING_ALLOWED_METHODS
+    XS_SHARING_ALLOWED_HEADERS = settings.XS_SHARING_ALLOWED_HEADERS
+    XS_SHARING_ALLOWED_CREDENTIALS = settings.XS_SHARING_ALLOWED_CREDENTIALS
+except AttributeError:
+    XS_SHARING_ALLOWED_ORIGINS = '*'
+    XS_SHARING_ALLOWED_METHODS = ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
+    XS_SHARING_ALLOWED_HEADERS = ['Content-Type', '*']
+    XS_SHARING_ALLOWED_CREDENTIALS = 'true'
+
+
+class XsSharing(object):
+    """
+    This middleware allows cross-domain XHR using the html5 postMessage API.
+     
+    Access-Control-Allow-Origin: http://foo.example
+    Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE
+
+    Based off https://gist.github.com/426829
+    """
+    def process_request(self, request):
+        if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
+            response = http.HttpResponse()
+            response['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS 
+            response['Access-Control-Allow-Methods'] = ",".join( XS_SHARING_ALLOWED_METHODS ) 
+            response['Access-Control-Allow-Headers'] = ",".join( XS_SHARING_ALLOWED_HEADERS )
+            response['Access-Control-Allow-Credentials'] = XS_SHARING_ALLOWED_CREDENTIALS
+            return response
+
+        return None
+
+    def process_response(self, request, response):
+        response['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS 
+        response['Access-Control-Allow-Methods'] = ",".join( XS_SHARING_ALLOWED_METHODS )
+        response['Access-Control-Allow-Headers'] = ",".join( XS_SHARING_ALLOWED_HEADERS )
+        response['Access-Control-Allow-Credentials'] = XS_SHARING_ALLOWED_CREDENTIALS
+
+        return response

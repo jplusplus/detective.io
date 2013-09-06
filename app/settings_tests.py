bf8b289af6596c74ade7e4b3ca5bda73df3b2443
Pierre Romera <hello@pirhoo.com>
2013-09-06 16:01:27 +0200

    Added travis test

---
+++ app/settings_tests.py
@@ -0,0 +1,48 @@
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+AUTHENTICATION_BACKENDS = ('neo4django.auth.backends.NodeModelBackend',)
+
+NEO4J_DATABASES = {
+    'default' : {
+        'HOST':'localhost',
+        'PORT':7474,
+        'ENDPOINT':'/db/data',
+        'OPTIONS':{
+            'CLEANDB_URI': '/cleandb/supersecretdebugkey!',
+        },
+    }
+}
+
+NEO4J_TEST_DATABASES = {
+    'default' : {
+        'HOST':'localhost',
+        'PORT':7474,
+        'ENDPOINT':'/db/data',
+        'OPTIONS':{
+            'CLEANDB_URI': '/cleandb/supersecretdebugkey!',
+        }
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db', 'test_database.sqlite3')
+    }
+}
+
+DATABASE_ROUTERS = ['neo4django.utils.Neo4djangoIntegrationRouter']
+
+USE_TZ = True
+
+INSTALLED_APPS = (
+    'app.detective',   
+)
+
+SECRET_KEY="shutupdjangowe'retryingtotesthere"
+
+DEBUG = True
+
+NEO4DJANGO_PROFILE_REQUESTS = False
+NEO4DJANGO_DEBUG_GREMLIN = False

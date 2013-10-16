5e9aa08ef0428100f320bd002a6ae2528ba130ce
Pierre Romera <hello@pirhoo.com>
2013-10-16 15:07:11 +0200

    Added jquery

--- app/settings.py
+++ app/settings.py
@@ -95,7 +95,8 @@ BOWER_INSTALLED_APPS = (
     "typeahead.js#0.9.3",
     "underscore#1.5.2",
     "unstable-angular-complete#1.1.5",
-    'git://github.com/kartograph/kartograph.js'
+    'git://github.com/kartograph/kartograph.js',
+    'jquery'
 )
 
 # List of finder classes that know how to find static files in

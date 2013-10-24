d2ff9e533d3bbc6989e0f1b95e984697eaed3469
Pierre Romera <hello@pirhoo.com>
2013-10-24 18:09:38 +0200

    Debug test for Django 1.5 #79

--- app/detective/tests/commands.py
+++ app/detective/tests/commands.py
@@ -1,33 +1,43 @@
 from django.core.management           import call_command
+from django.core.management.base      import CommandError
 from django.test                      import TestCase
 from StringIO                         import StringIO 
-from neo4django.graph_auth.models           import User
+from neo4django.graph_auth.models     import User
 from app.detective.apps.common.models import Country
+
 import sys
- 
+
+
 
 class CommandsTestCase(TestCase):
+
+    def test_parseowl_fail(self):
+        # Catch output
+        output = StringIO()
+        sys.stdout = output
+        # Must fail without argument
+        with self.assertRaises(CommandError):
+            call_command('parseowl')
 
     def test_parseowl(self):
         # Catch output
         output = StringIO()
         sys.stdout = output
-        # Must fail without argument
-        with self.assertRaises(SystemExit):
-            call_command('parseowl')
-
         args = "./app/data/ontology-v5.3.owl"
         call_command('parseowl', args)
 
+    def test_createsuperuser_fail(self):
+        # Catch output
+        output = StringIO()
+        sys.stdout = output
+        # Must fail without argument
+        with self.assertRaises(CommandError):
+            call_command('createsuperuser', interactive=False)
 
     def test_createsuperuser(self):
         # Catch output
         output = StringIO()
         sys.stdout = output
-        # Must fail without argument
-        with self.assertRaises(SystemExit):
-            call_command('createsuperuser', interactive=False)
-
         # Create the user
         call_command('createsuperuser', username="supertest", password="supersecret", email="super@email.com")
         # Check that it exists
@@ -35,13 +45,18 @@ class CommandsTestCase(TestCase):
         self.assertEqual(user.is_superuser, True)
 
 
-    def test_loadnodes(self):
+    def test_loadnodes_fail(self):
         # Catch output
         output = StringIO()
         sys.stdout = output
         # Must fail without argument
-        with self.assertRaises(SystemExit):
+        with self.assertRaises(CommandError):
             call_command('loadnodes')
+
+    def test_loadnodes(self):
+        # Catch output
+        output = StringIO()
+        sys.stdout = output
         # Import countries
         args = "./app/detective/apps/common/fixtures/countries.json"
         call_command('loadnodes', args)

ca1fc2c834b52222b94e84b097840386d39ee0ac
Bellon Pierre <bellon.pierre@gmail.com>
2013-11-12 10:48:37 +0100

    Added reset_password & reset_password_confirm method to User ressource

--- app/detective/apps/common/user.py
+++ app/detective/apps/common/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from django.core                  import signing
+from django.core.urlresolvers     import reverse
 from django.conf.urls             import url
 from django.contrib.auth          import authenticate, login, logout
 from django.contrib.auth.models   import User
@@ -11,6 +13,8 @@ from tastypie.resources           import ModelResource
 from tastypie.utils               import trailing_slash
 from django.contrib.auth.hashers  import make_password
 
+from password_reset.views import Reset
+from .message import Recover 
 
 class UserAuthorization(Authorization):
     def read_detail(self, object_list, bundle):
@@ -40,9 +44,11 @@ class UserResource(ModelResource):
     def prepend_urls(self):
         params = (self._meta.resource_name, trailing_slash())
         return [
-            url(r"^(?P<resource_name>%s)/login%s$"  % params, self.wrap_view('login'), name="api_login"),
-            url(r'^(?P<resource_name>%s)/logout%s$' % params, self.wrap_view('logout'), name='api_logout'),
-            url(r'^(?P<resource_name>%s)/status%s$' % params, self.wrap_view('status'), name='api_status'),
+            url(r"^(?P<resource_name>%s)/login%s$"                  % params, self.wrap_view('login'),                  name="api_login"),
+            url(r'^(?P<resource_name>%s)/logout%s$'                 % params, self.wrap_view('logout'),                 name='api_logout'),
+            url(r'^(?P<resource_name>%s)/status%s$'                 % params, self.wrap_view('status'),                 name='api_status'),
+            url(r'^(?P<resource_name>%s)/reset_password%s$'         % params, self.wrap_view('reset_password'),         name='api_reset_password'),
+            url(r'^(?P<resource_name>%s)/reset_password_confirm%s$' % params, self.wrap_view('reset_password_confirm'), name='api_reset_password_confirm'),
         ]
 
     def login(self, request, **kwargs):
@@ -115,3 +121,39 @@ class UserResource(ModelResource):
             return self.create_response(request, { 'is_logged': True,  'username': request.user.username })
         else:
             return self.create_response(request, { 'is_logged': False, 'username': '' })
+
+    def reset_password(self, request, **kwargs):
+        """
+        Send the reset password email to user with the proper URL.
+        """ 
+        self.method_check(request, allowed=['post'])
+        data    = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+        user    = User.objects.get(email=data['email'])
+        recover = Recover()
+        recover.user = user
+        recover.request = request
+        recover.email_template_name = 'reset-password.html'
+        recover.send_notification()
+
+        return self.create_response(request, { 'success': True })
+
+    def reset_password_confirm(self, request, **kwargs):
+        """
+        Reset the password if the POST's token parameter is a valid token
+        """
+        self.method_check(request, allowed=['post'])
+        reset        = Reset()
+        data         = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+        tok          = data['token']
+        raw_password = data['password']
+        try:
+            pk = signing.loads(tok, max_age=reset.token_expires,salt=reset.salt)
+        except signing.BadSignature:
+            return self.create_response(request, { 'success': False, 'error_message': 'Wrong signature, your token may had expired (valid for 48 hours)' })
+        
+        user = User.objects.get(pk=pk)
+        user.set_password(raw_password)
+        return self.create_response(request, { 'success': True })
+
+
+

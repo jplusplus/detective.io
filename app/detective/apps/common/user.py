a28453470165169c3a58831b81387306f7f96bd9
Bellon Pierre <bellon.pierre@gmail.com>
2013-11-12 17:25:37 +0100

    Changed template name for password reset emailing

--- app/detective/apps/common/user.py
+++ app/detective/apps/common/user.py
@@ -189,7 +189,7 @@ class UserResource(ModelResource):
         recover = Recover()
         recover.user = user
         recover.request = request
-        recover.email_template_name = 'reset-password.html'
+        recover.email_template_name = 'email-reset-password.html'
         recover.send_notification()
 
         return self.create_response(request, { 'success': True })

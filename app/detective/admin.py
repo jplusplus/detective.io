235970dea38c82b329ac90c0f68c12fa39ec7484
Pierre Romera <hello@pirhoo.com>
2013-11-28 18:50:58 +0100

    Added topic models

--- app/detective/admin.py
+++ app/detective/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from app.detective.models import QuoteRequest
+from app.detective.models import QuoteRequest, Topic
 
 class QuoteRequestAdmin(admin.ModelAdmin):
     list_filter = ("employer", "records", "users", "public", )
     search_fields = ("name", "employer", "domain", "email", "comment",)
 
 admin.site.register(QuoteRequest, QuoteRequestAdmin)
+
+class TopicAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ("title", "link", "public", )
+admin.site.register(Topic, TopicAdmin)
--- app/detective/models.py
+++ app/detective/models.py
@@ -1,4 +1,6 @@
-from django.db import models
+from .utils                   import get_apps
+from django.core.urlresolvers import reverse
+from django.db                import models
 
 class QuoteRequest(models.Model):
     RECORDS_SIZE = (
@@ -30,3 +32,22 @@ class QuoteRequest(models.Model):
 
     def __unicode__(self):
         return "%s - %s" % (self.name, self.email,)
+
+class Topic(models.Model):
+    MODULES     = tuple( (app, app,) for app in get_apps() )
+    title       = models.CharField(max_length=250, help_text="Title of your topic.")
+    module      = models.CharField(max_length=250, choices=MODULES, unique=True, help_text="Module to use to create your topic.")
+    slug        = models.SlugField(max_length=250, unique=True, help_text="Token to use into the url.")
+    description = models.TextField(null=True, blank=True, help_text="A short description of what your topic.")
+    public      = models.BooleanField(help_text="Is your app public?", default=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_path(self):
+        return reverse('explore', args=[self.slug])
+
+    def link(self):
+        path = self.get_absolute_path()
+        return '<a href="%s">%s</a>' % (path, path, )
+    link.allow_tags = True

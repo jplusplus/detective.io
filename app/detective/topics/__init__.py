11bb591b836b2c9afdd9e3e0dab1c79a02ad1512
Pierre Romera <hello@pirhoo.com>
2013-12-17 17:20:50 +0100

    Added energy fixtures

--- app/detective/topics/__init__.py
+++ app/detective/topics/__init__.py
@@ -1,3 +1,16 @@
-# -*- coding: utf-8 -*-
 from app.detective.register import init_topics
-init_topics()
+import sys
+# -*- coding: utf-8 -*-
+class Wrapper(object):
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        init_topics()
+
+    def __getattr__(self, name):
+        try:
+            return getattr(self.wrapped, name)
+        except AttributeError:
+            print "noob"
+
+sys.modules[__name__] = Wrapper( sys.modules[__name__] )

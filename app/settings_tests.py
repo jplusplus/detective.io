ad9a5caffff9a3fb4df29c14fb3b26cddd02568c
Edouard <edou4rd@gmail.com>
2014-01-06 10:44:50 +0100

    SOUTH_TESTS_MIGRATE removed from settings_tests.py

--- app/settings_tests.py
+++ app/settings_tests.py
@@ -28,9 +28,4 @@ INSTALLED_APPS.remove('djrill')
 NEO4DJANGO_PROFILE_REQUESTS = False
 NEO4DJANGO_DEBUG_GREMLIN = False
 
-# By default, South’s syncdb command will also apply migrations if it’s run in non-interactive mode, 
-# which includes when you’re running tests - it will run every migration every time you run your tests.
-# See http://south.readthedocs.org/en/latest/unittests.html#unit-test-integration
-SOUTH_TESTS_MIGRATE = False
-
 # EOF

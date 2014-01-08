690d37e51184816193c1843cf85d69b61576b436
Edouard <edou4rd@gmail.com>
2014-01-08 16:44:43 +0100

    Logger : formatter verbose removed

--- app/settings.py
+++ app/settings.py
@@ -216,9 +216,6 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
         'simple': {
             'format': '[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s'
         },

810cdaae36e4346bf3c654f85b8b7c5121a498c5
Edouard <edou4rd@gmail.com>
2014-01-21 17:08:32 +0100

    Job: in default queue, result as a json stringified

--- app/detective/topics/common/job.py
+++ app/detective/topics/common/job.py
@@ -14,29 +14,32 @@ from tastypie.resources import Resource
 from tastypie           import fields
 from rq.job             import Job
 import django_rq
+import json
 
 class Document(object):
     def __init__(self, *args, **kwargs):
         self._id = None
         for key, value in kwargs.iteritems():
             setattr(self, key, value)
+        if hasattr(self,'_result') and self._result:
+            result = json.dumps(self._result)
+            self._result = result
 
 class JobResource(Resource):
     id         = fields.CharField(attribute="_id")
-    result     = fields.CharField(attribute="_result", null=True)
-    meta       = fields.CharField(attribute="meta", null=True)
-    status     = fields.CharField(attribute="_status", null=True)
-    created_at = fields.CharField(attribute="created_at", null=True)
-    timeout    = fields.CharField(attribute="timeout", null=True)
+    result     = fields.CharField(attribute="_result"    , null=True)
+    meta       = fields.CharField(attribute="meta"       , null=True)
+    status     = fields.CharField(attribute="_status"    , null=True)
+    created_at = fields.CharField(attribute="created_at" , null=True)
+    timeout    = fields.CharField(attribute="timeout"    , null=True)
 
     def obj_get(self, bundle, **kwargs):
         """
         Returns redis document from provided id.
         """
-        queue = django_rq.get_queue('high')
+        queue = django_rq.get_queue('default')
         job = Job.fetch(kwargs['pk'], connection=queue.connection)
         return Document(**job.__dict__)
-        return []
 
     class Meta:
         resource_name          = "jobs"

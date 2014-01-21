e8a16e96fe8898234508729be9f4d96ffb94aaf1
Pierre Romera <hello@pirhoo.com>
2014-01-21 15:01:28 +0200

    Added save_as_top

--- app/detective/admin.py
+++ app/detective/admin.py
@@ -1,24 +1,19 @@
 from django.contrib       import admin
-from django               import forms
 from app.detective.models import QuoteRequest, Topic, RelationshipSearch, Article
 
 class QuoteRequestAdmin(admin.ModelAdmin):
-    list_filter = ("employer", "records", "users", "public", )
+    save_on_top   = True
+    list_filter   = ("employer", "records", "users", "public", )
     search_fields = ("name", "employer", "domain", "email", "comment",)
 
 admin.site.register(QuoteRequest, QuoteRequestAdmin)
 
-
-class RelationshipSearchForm(forms.ModelForm):
-    class Meta:
-        pass
-
 class RelationshipSearchInline(admin.TabularInline):
     model = RelationshipSearch
-    form  = RelationshipSearchForm
     extra = 1
 
 class TopicAdmin(admin.ModelAdmin):
+    save_on_top         = True
     prepopulated_fields = {'slug': ('title',)}
     list_display        = ("title", "link", "public", )
     inlines             = (RelationshipSearchInline,)
@@ -36,7 +31,8 @@ class TopicAdmin(admin.ModelAdmin):
 admin.site.register(Topic, TopicAdmin)
 
 class ArticleAdmin(admin.ModelAdmin):
+    save_on_top         = True
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ("title", "link", "created_at", "public", )
+    list_display        = ("title", "link", "created_at", "public", )
 
 admin.site.register(Article, ArticleAdmin)

36bdf95292acdca206c7165877fe4a920ecc2347
Edouard <edou4rd@gmail.com>
2014-01-24 13:00:22 +0100

    don't create permission on topic updated

--- app/detective/models.py
+++ app/detective/models.py
@@ -108,10 +108,6 @@ class Topic(models.Model):
         # Ensure that the module field is populated with app_label()
         self.module = self.app_label()
         models.Model.save(self)
-        # Then create the permissions related to the label module
-        # @TODO check that the slug changed or not to avoid permissions hijacking
-        # FIXME: don't understand why the app_module.models module is used here
-        create_permissions(self.get_models_module(), app_label=self.slug)
 
     def has_default_ontology(self):
         module = self.get_module()
@@ -168,6 +164,14 @@ class RelationshipSearch(models.Model):
 #
 # -----------------------------------------------------------------------------
 from django.db.models import signals
+
+def update_permissions(sender, instance, created, raw, using, update_fields, **kwargs):
+    # Then create the permissions related to the label module
+    # @TODO check that the slug changed or not to avoid permissions hijacking
+    if created:
+        create_permissions(instance.get_module(), app_label=instance.slug)
+
 signals.post_delete.connect(remove_permissions, sender=Topic)
+signals.post_save.connect(update_permissions, sender=Topic)
 
 # EOF

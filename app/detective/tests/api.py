f5a5070cf1410471a6c2bc8cda5c4d8deeb5d662
Pierre Romera <hello@pirhoo.com>
2014-01-29 23:33:20 +0100

    Fixed test

--- app/detective/tests/api.py
+++ app/detective/tests/api.py
@@ -154,7 +154,7 @@ class ApiTestCase(ResourceTestCase):
             },
             "predicate": {
                 "label": u"has activity in",
-                "name": u"activity_in_organization",
+                "name": u"activity_in",
                 "subject": u"Person"
             },
             "subject": {

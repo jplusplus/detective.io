4c36c912d6fdc05477efb5579a50a45419fa62d6
Paul Joannon <paul.joannon@gmail.com>
2014-07-04 16:16:46 +0200

    set is_editable to False for 'in' relations on same model

--- app/detective/register.py
+++ app/detective/register.py
@@ -81,7 +81,8 @@ def default_rules(topic):
                 # Set it into the rules
                 rules.model(model).field(field.name).add(is_searchable=modelRules["is_searchable"])
                 # Entering relationship are not editable yet
-                rules.model(model).field(field.name).add(is_editable=modelRules["is_editable"])
+                is_editable = False if field.direction == 'in' and target_model is model else modelRules["is_editable"]
+                rules.model(model).field(field.name).add(is_editable=is_editable)
     return rules
 
 def import_or_create(path, register=True, force=False):

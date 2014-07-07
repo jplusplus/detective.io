2a69d32fa27015eb665b5f73de22d7e957a94290
Edouard <edou4rd@gmail.com>
2014-07-07 16:34:26 +0200

    Migration 0011 : data included, no more fixtures file

--- app/detective/migrations/0011_load_myfixture.py
+++ app/detective/migrations/0011_load_myfixture.py
@@ -1,17 +1,305 @@
 # -*- coding: utf-8 -*-
 from south.v2    import DataMigration
-import json
 
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        json_data=open("app/detective/fixtures/search_terms.json")
-        search_terms = json.load(json_data)
-        for st in search_terms:
+        fixtures = [
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "personal_payer",
+                    "subject": "FundraisingRound",
+                    "label": "was financed by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "payer",
+                    "subject": "FundraisingRound",
+                    "label": "was financed by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "educated_in",
+                    "subject": "Person",
+                    "label": "was educated in"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "based_in",
+                    "subject": "Person",
+                    "label": "is based in"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "activity_in_organization",
+                    "subject": "Person",
+                    "label": "has activity in"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "previous_activity_in_organization",
+                    "subject": "Person",
+                    "label": "had previous activity in"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "price",
+                    "subject": "EnergyProduct",
+                    "label": "is sold at"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "author",
+                    "subject": "Commentary",
+                    "label": "was written by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "distribution",
+                    "subject": "EnergyProduct",
+                    "label": "is distributed in"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "operator",
+                    "subject": "EnergyProduct",
+                    "label": "is operated by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "price",
+                    "subject": "EnergyProduct",
+                    "label": "is sold at"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "adviser",
+                    "subject": "Organization",
+                    "label": "is advised by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "key_person",
+                    "subject": "Organization",
+                    "label": "is staffed by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "partner",
+                    "subject": "Organization",
+                    "label": "has a partnership with"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "fundraising_round",
+                    "subject": "Organization",
+                    "label": "was financed by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "monitoring_body",
+                    "subject": "Organization",
+                    "label": "is monitored by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "litigation_against",
+                    "subject": "Organization",
+                    "label": "has a litigation against"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "revenue",
+                    "subject": "Organization",
+                    "label": "has revenue of"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "board_member",
+                    "subject": "Organization",
+                    "label": "has board of directors with"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "commentary",
+                    "subject": "EnergyProject",
+                    "label": "is analyzed by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "owner",
+                    "subject": "EnergyProject",
+                    "label": "is owned by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "partner",
+                    "subject": "EnergyProject",
+                    "label": "has a partnership with"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "activity_in_country",
+                    "subject": "EnergyProject",
+                    "label": "has activity in"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "activity_in_country",
+                    "subject": "Distribution",
+                    "label": "has activity in"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "product",
+                    "subject": "EnergyProject",
+                    "label": "has product of"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "commentary",
+                    "subject": "EnergyProject",
+                    "label": "is analyzed by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "owner",
+                    "subject": "EnergyProject",
+                    "label": "is owned by"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "partner",
+                    "subject": "EnergyProject",
+                    "label": "has partnership with"
+                }
+            },
+            {
+                "pk": None,
+                "model": "detective.searchterm",
+                "fields": {
+                    "topic": 2,
+                    "name": "activity_in_country",
+                    "subject": "EnergyProject",
+                    "label": "has activity in"
+                }
+            }
+        ]
+        for st in fixtures:
             st["fields"]["topic"] = orm["detective.topic"].objects.get(id=st["fields"]["topic"])
             obj = orm["detective.searchterm"](**st["fields"])
             obj.save()
-        json_data.close()
 
     def backwards(self, orm):
         "Write your backwards methods here."

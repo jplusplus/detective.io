47af53a3de16b7342393718d67ca69ecee910afc
Paul Joannon <paul.joannon@gmail.com>
2014-07-18 17:10:07 +0200

    remove new stuffs from owl

--- app/detective/parser/owl.py
+++ app/detective/parser/owl.py
@@ -38,7 +38,7 @@ def attr(obj, name, default=None):
         return obj.get("{%s}%s" % ( NAMESPACES[tokens[0]], tokens[1]), default)
 
 def get_field_specials(root, field_name):
-    specials = ["verbose_name", "help_text", "related_name", "default"]
+    specials = ["verbose_name", "help_text", "related_name"]
     props = {}
     tags  = root.findall("owl:ObjectProperty",   namespaces=NAMESPACES)
     tags += root.findall("owl:DatatypeProperty", namespaces=NAMESPACES)
@@ -139,8 +139,6 @@ def parse(ontology, module='', app_label=None):
                 if field_type is None: continue
                 # Convert the type to a python readable format
                 field_type = OWLTYPES[attr(field_type, "rdf:resource").split("#")[-1]]
-            if field_type == 'BooleanProperty' and not 'default' in field_opts.keys():
-                field_opts['default'] = False
             # Record the field
             class_fields[field_name] = getattr(models, field_type)(**field_opts)
         # Record the class with this fields

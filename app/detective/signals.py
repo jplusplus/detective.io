614b2ce125d74b04f0f3765ec9c90f1416987925
pbellon <bellon.pierre@gmail.com>
2014-08-04 14:48:18 +0200

    Fixed topics' sub models signal binding

--- app/detective/signals.py
+++ app/detective/signals.py
@@ -1,7 +1,16 @@
 from django.db.models           import signals
-from app.detective.utils        import topic_cache
+from app.detective.utils        import topic_cache, get_topic_from_model
 from app.detective.models       import Topic
 from app.detective.permissions  import create_permissions, remove_permissions
+
+
+def _extra_args(topic, *args, **kwargs):
+    def inner1(f, *args, **kwargs):
+        def inner2(sender, instance, **kwargs):
+            f(sender, instance, topic=topic, **kwargs)
+        return inner2
+    return inner1
+
 
 def update_permissions(*args, **kwargs):
     """ create the permissions related to the label module """
@@ -10,15 +19,27 @@ def update_permissions(*args, **kwargs):
     if kwargs.get('created', False):
         create_permissions(kwargs.get('instance').get_module(), app_label=kwargs.get('instance').ontology_as_mod)
 
-def update_topic_cache(*args, **kwargs):
-    topic = kwargs.get('instance')
+def update_topic_cache(topic=None, *args, **kwargs):
+    instance = kwargs.get('instance')
+    if not isinstance(instance, Topic):
+        topic = get_topic_from_model(instance)
+    else:
+        topic = instance
     topic_cache.incr_version(topic)
+
+    # if topic just been created we gonna bind its sub models signals
+    if kwargs.get('created'):
+        for Model in topic.get_models():
+            signals.post_save.connect(update_topic_cache, sender=Model, weak=False )
+
 
 def remove_topic_cache(*args, **kwargs):
     topic_cache.delete_version(kwargs.get('instance'))
 
 def bind():
     signals.post_save.connect(update_topic_cache,   sender=Topic)
+    signals.post_save.connect(update_permissions,   sender=Topic)
     signals.pre_delete.connect(remove_topic_cache,  sender=Topic)
     signals.post_delete.connect(remove_permissions, sender=Topic)
-    signals.post_save.connect(update_permissions,   sender=Topic)
+
+

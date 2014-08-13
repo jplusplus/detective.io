50effd1ec6f30d26ac5a67ed0e8d4a023bc5f10a
pbellon <bellon.pierre@gmail.com>
2014-08-13 18:09:39 +0200

    Added import of detective.topics in __init__ to force virtual module init

--- app/detective/__init__.py
+++ app/detective/__init__.py
@@ -1,4 +1,6 @@
+from app.detective import topics # will init virtual modules
 from app.detective import signals, utils
+
 from django.conf   import settings
 settings.INSTALLED_APPS += utils.get_topics_modules()
 

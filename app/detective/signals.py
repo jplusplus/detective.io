f78887a6f6cfcbba51ba7b6aea02388fd5ce5183
pbellon <bellon.pierre@gmail.com>
2014-08-13 17:21:06 +0200

    Cleaned models code (removed duplicated signals & moved binding to signals.py bind method

--- app/detective/signals.py
+++ app/detective/signals.py
@@ -1,7 +1,12 @@
 from django.db.models           import signals
+from django.contrib.auth        import get_user_model
+from django.db.utils            import DatabaseError
 from app.detective.utils        import topic_cache, get_topic_from_model
-from app.detective.models       import Topic
+from app.detective.models       import Topic, DetectiveProfileUser
 from app.detective.permissions  import create_permissions, remove_permissions
+
+User = get_user_model()
+
 
 def update_permissions(*args, **kwargs):
     """ create the permissions related to the label module """
@@ -28,17 +33,37 @@ def update_topic_cache(*args, **kwargs):
 
         # if topic just been created we gonna bind its sub models signals
         if kwargs.get('created'):
-            for Model in topic.get_models():
-                signals.post_save.connect(update_topic_cache, sender=Model, weak=False )
-
+            bind_topic_models(topic)
 
 def remove_topic_cache(*args, **kwargs):
     topic_cache.delete_version(kwargs.get('instance'))
 
+def bind_topic_models(topic):
+    for Model in topic.get_models():
+        signals.post_save.connect(update_topic_cache, sender=Model, weak=False )
+
+def user_created(*args, **kwargs):
+    """
+
+    create a DetectiveProfileUser when a user is created
+
+    """
+    DetectiveProfileUser.objects.get_or_create(user=kwargs.get('instance'))
+
+
 def bind():
+    signals.post_save.connect(user_created,         sender=User)
     signals.post_save.connect(update_topic_cache,   sender=Topic)
     signals.post_save.connect(update_permissions,   sender=Topic)
     signals.pre_delete.connect(remove_topic_cache,  sender=Topic)
     signals.post_delete.connect(remove_permissions, sender=Topic)
+
+    try:
+        # for existing topics we need to bind their submodels
+        for topic in Topic.objects.all():
+            bind_topic_models(topic)
+    except DatabaseError:
+        pass
+
 
 

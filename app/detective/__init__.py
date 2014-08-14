558347fb439aa341b5aca62a46a9bc168b173eee
pbellon <bellon.pierre@gmail.com>
2014-08-14 11:51:50 +0200

    Revert "Changed order of app.detective.__init__ processed"
    This reverts commit 875dffacd42c77699d4387d967445982e03d8e2e.

--- app/detective/__init__.py
+++ app/detective/__init__.py
@@ -1,6 +1,5 @@
 from app.detective import signals, utils
 from django.conf   import settings
-settings.INSTALLED_APPS += utils.get_topics_modules()
-
 signals.bind()
 
+settings.INSTALLED_APPS += utils.get_topics_modules()

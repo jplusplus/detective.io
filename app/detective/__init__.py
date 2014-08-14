3dd5ef32143796207b538074f25b866f436f07fe
pbellon <bellon.pierre@gmail.com>
2014-08-14 11:51:07 +0200

    Revert "Added import of detective.topics in __init__ to force virtual module init"
    This reverts commit 50effd1ec6f30d26ac5a67ed0e8d4a023bc5f10a.

--- app/detective/__init__.py
+++ app/detective/__init__.py
@@ -1,6 +1,4 @@
-from app.detective import topics # will init virtual modules
 from app.detective import signals, utils
-
 from django.conf   import settings
 settings.INSTALLED_APPS += utils.get_topics_modules()
 

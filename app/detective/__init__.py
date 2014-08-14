7ef2f199daa958d8590808c699a50245d3c33f44
pbellon <bellon.pierre@gmail.com>
2014-08-14 11:50:44 +0200

    Revert "Commented dynamite __init__ file, please be careful"
    This reverts commit cc875371aea3c4073c3deb8662a8572f8e0d118f.

--- app/detective/__init__.py
+++ app/detective/__init__.py
@@ -1,14 +1,9 @@
-# Warning: Edit this file with caution. It is highly sensitive and could cause
-# detective.io misbehavior and many API endpoints could produce errors and
-# failures
 from django.conf   import settings
 from .signals import bind
 from .utils   import get_topics_modules
 
-# first we add topics as modules to our installed apps
 settings.INSTALLED_APPS += get_topics_modules()
-# we now import topics file which will wrap topics as modules
-import app.detective.topics
-# then we bind application signals
+
+import app.detective.topics # will init virtual modules
 bind()
 

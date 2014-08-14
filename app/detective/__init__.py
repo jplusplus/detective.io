41e50af7228e2804b1469b2bf1d4ff1a15a2f5cc
Edouard <edou4rd@gmail.com>
2014-08-14 11:43:25 +0200

    connect signal on models

--- app/detective/__init__.py
+++ app/detective/__init__.py
@@ -1,5 +1,4 @@
-from app.detective import signals, utils
+from app.detective import utils
 from django.conf   import settings
-signals.bind()
 
 settings.INSTALLED_APPS += utils.get_topics_modules()

db910c0e3f0f3f25ad7c8f6e240fc06a88cfae43
Paul Joannon <paul.joannon@gmail.com>
2014-08-19 18:33:56 +0200

    move common topic to detective's ownership

--- app/detective/urls.py
+++ app/detective/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('api',
     # Energy and Common are the 2 first topics and are threat with attentions
-    url(r'^common/', include('app.detective.topics.common.urls', namespace='common')),
-    url(r'^energy/', include('app.detective.topics.energy.urls', namespace='energy')),
+    url(r'^detective/common/', include('app.detective.topics.common.urls', namespace='common')),
+    url(r'^detective/energy/', include('app.detective.topics.energy.urls', namespace='energy')),
 )

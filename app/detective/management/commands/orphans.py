fd1e849c87b6b57fd540b13215445c01973c704e
Edouard <edou4rd@gmail.com>
2014-09-05 14:32:28 +0200

    try catch orphans.py update

--- app/detective/management/commands/orphans.py
+++ app/detective/management/commands/orphans.py
@@ -28,29 +28,32 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        for topic in Topic.objects.all():
-            # escape common & energy as usual
-            if topic.slug in ["common", "energy"]: continue
-            self.stdout.write("Topic: %s" % (topic))
-            orphans_count = 0
-            for Model in topic.get_models():
-                fields = utils.get_model_fields(Model)
-                for field in fields:
-                    if field["related_model"] and field["direction"] == "out" and "through" in field["rules"]:
-                        ids= []
-                        try:
-                            for Model in Model.objects.all():
-                                ids.extend([_.id for _ in Model.node.relationships.all()])
-                            Properties = field["rules"]["through"]
-                            for info in Properties.objects.all():
-                                if info._relationship not in ids:
-                                    self.stdout.write("\t%s is an orphelin property of the model %s. The relation doesn't exist no more." % (info._NodeModel__node, Model.__class__.__name__))
-                                    orphans_count += 1
-                                    if options["fix"]:
-                                        self.stdout.write("\tremoving %s" % (info))
-                                        info.delete()
-                        except Exception as e:
-                            self.stderr.write("\tError with model %s (%s)" % (Model.__class__.__name__, e))
-            self.stdout.write("\tfound %d orphans" % (orphans_count))
+        try:
+            for topic in Topic.objects.all():
+                # escape common & energy as usual
+                if topic.slug in ["common", "energy"]: continue
+                self.stdout.write("Topic: %s" % (topic))
+                orphans_count = 0
+                for Model in topic.get_models():
+                    try:
+                        fields = utils.get_model_fields(Model)
+                        for field in fields:
+                            if field["related_model"] and field["direction"] == "out" and "through" in field["rules"]:
+                                ids= []
+                                for Model in Model.objects.all():
+                                    ids.extend([_.id for _ in Model.node.relationships.all()])
+                                Properties = field["rules"]["through"]
+                                for info in Properties.objects.all():
+                                    if info._relationship not in ids:
+                                        self.stdout.write("\t%s is an orphelin property of the model %s. The relation doesn't exist no more." % (info._NodeModel__node, Model.__class__.__name__))
+                                        orphans_count += 1
+                                        if options["fix"]:
+                                            self.stdout.write("\tremoving %s" % (info))
+                                            info.delete()
+                    except Exception as e:
+                        self.stderr.write("\tError with model %s (%s)" % (Model.__class__.__name__, e))
+                self.stdout.write("\tfound %d orphans" % (orphans_count))
+        except Exception as e:
+            self.stderr.write("\tError with model %s (%s)" % (Model.__class__.__name__, e))
 
 # EOF

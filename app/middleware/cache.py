9055d17b48ea8a8023f9bb67ad379d0bd5e67e2a
Edouard <edou4rd@gmail.com>
2014-10-01 17:48:58 +0200

    Cache middleware

--- app/middleware/cache.py
+++ app/middleware/cache.py
@@ -18,8 +18,11 @@ class FetchFromCacheMiddleware(object):
         self.cache_alias = settings.CACHE_MIDDLEWARE_ALIAS
         self.cache = get_cache(self.cache_alias)
 
-
-    def _should_update_cache(self, request):
+    def _should_fetch_from_cache(self, request):
+        for regex in getattr(settings, 'CACHE_BYPASS_URLS', []):
+            request_url = request.get_full_path()
+            if re.match(regex, request_url):
+                return False
         if self.cache_anonymous_only:
             assert hasattr(request, 'user'), "The Django cache middleware with CACHE_MIDDLEWARE_ANONYMOUS_ONLY=True requires authentication middleware to be installed. Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.auth.middleware.AuthenticationMiddleware' before the CacheMiddleware."
             if request.user.is_authenticated():
@@ -32,15 +35,9 @@ class FetchFromCacheMiddleware(object):
         Checks whether the page is already cached and returns the cached
         version if available.
         """
-        if not request.method in ('GET', 'HEAD') or not self._should_update_cache(request):
+        if not request.method in ('GET', 'HEAD') or not self._should_fetch_from_cache(request):
             request._cache_update_cache = False
             return None # Don't bother checking the cache.
-
-        for regex in getattr(settings, 'CACHE_BYPASS_URLS', []):
-            request_url = request.get_full_path()
-            if re.match(regex, request_url):
-                request._cache_update_cache = False
-                return None
 
         # try and get the cached GET response
         cache_key = get_cache_key(request, self.key_prefix, 'GET', cache=self.cache)

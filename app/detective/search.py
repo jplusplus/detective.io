fc410020eb1cdda6e4e29f2733fd9d827ef99e6c
pirhoo <pierre.romera@gmail.com>
2014-10-07 19:00:45 -0400

    Moved search function from summary to search class

--- app/detective/search.py
+++ app/detective/search.py
@@ -1,25 +1,215 @@
-from neo4django.db import connection
+from neo4django.db        import connection
+from app.detective.models import SearchTerm
+from difflib              import SequenceMatcher
 import re
 
-def by_name(terms, app_label):
-    if type(terms) in [str, unicode]:
-        terms = [terms]
-    matches = []
-    for term in terms:
-        term = unicode(term).lower()
-        term = re.sub("\"|'|`|;|:|{|}|\|(|\|)|\|", '', term).strip()
-        matches.append("LOWER(node.name) =~ '.*(%s).*'" % term)
-    # Query to get every result
-    query = """
-        START root=node(0)
-        MATCH (node)<-[r:`<<INSTANCE>>`]-(type)<-[`<<TYPE>>`]-(root)
-        WHERE HAS(node.name) """
-    if matches:
-        query += """
-        AND (%s) """ % ( " OR ".join(matches))
-    query += """
-        AND type.app_label = '%s'
-        RETURN ID(node) as id, node.name as name, type.model_name as model
-    """ % (app_label)
+class Search(object):
 
-    return connection.cypher(query).to_dicts()
+    def __init__(self, topic):
+        self.topic = topic
+
+    def by_name(self, terms):
+        if type(terms) in [str, unicode]:
+            terms = [terms]
+        matches = []
+        for term in terms:
+            term = unicode(term).lower()
+            term = re.sub("\"|'|`|;|:|{|}|\|(|\|)|\|", '', term).strip()
+            matches.append("LOWER(node.name) =~ '.*(%s).*'" % term)
+        # Query to get every result
+        query = """
+            START root=node(0)
+            MATCH (node)<-[r:`<<INSTANCE>>`]-(type)<-[`<<TYPE>>`]-(root)
+            WHERE HAS(node.name) """
+        if matches:
+            query += """
+            AND (%s) """ % ( " OR ".join(matches))
+        query += """
+            AND type.app_label = '%s'
+            RETURN ID(node) as id, node.name as name, type.model_name as model
+        """ % (self.topic.app_label())
+
+        return connection.cypher(query).to_dicts()
+
+
+    def ngrams(self, input):
+        input = input.split(' ')
+        output = []
+        end = len(input)
+        for n in range(1, end+1):
+            for i in range(len(input)-n+1):
+                output.append(input[i:i+n])
+        return output
+
+    def get_close_labels(self, token, lst, ratio=0.6):
+        """
+            Look for the given token into the list using labels
+        """
+        matches = []
+        for item in lst:
+            cpr = item["label"]
+            relevance = SequenceMatcher(None, token, cpr).ratio()
+            if relevance >= ratio:
+                item["relevance"] = relevance
+                matches.append(item)
+        return matches
+
+    def find_matches(self, query):
+        # Group ngram by following string
+        ngrams  = [' '.join(x) for x in self.ngrams(query) ]
+        matches = []
+        models  = self.get_syntax().get("subject").get("model")
+        rels    = self.get_syntax().get("predicate").get("relationship")
+        lits    = self.get_syntax().get("predicate").get("literal")
+        # Known models lookup for each ngram
+        for token in ngrams:
+            obj = {
+                'models'       : self.get_close_labels(token, models),
+                'relationships': self.get_close_labels(token, rels),
+                'literals'     : self.get_close_labels(token, lits),
+                'token'        : token
+            }
+            matches.append(obj)
+        return matches
+
+    def build_propositions(self, matches, query):
+        """
+            For now, a proposition follow the form
+            <subject> <predicat> <object>
+            Where a <subject>, is an "Named entity" or a Model
+            a <predicat> is a relationship type
+            and an <object> is a "Named entity" or a Model.
+            Later, as follow RDF standard, an <object> could be any data.
+        """
+        def remove_duplicates(lst):
+            seen = set()
+            new_list = []
+            for item in lst:
+                if type(item) is dict:
+                    # Create a hash of the dictionary
+                    obj = hash(frozenset(item.items()))
+                else:
+                    obj = hash(item)
+                if obj not in seen:
+                    seen.add(obj)
+                    new_list.append(item)
+            return new_list
+
+        def is_preposition(token=""):
+            return unicode(token).lower() in ["aboard", "about", "above", "across", "after", "against",
+            "along", "amid", "among", "anti", "around", "as", "at", "before", "behind", "below",
+            "beneath", "beside", "besides", "between", "beyond", "but", "by", "concerning",
+            "considering",  "despite", "down", "during", "except", "excepting", "excluding",
+            "following", "for", "from", "in", "inside", "into", "like", "minus", "near", "of",
+            "off", "on", "onto", "opposite", "outside", "over", "past", "per", "plus", "regarding",
+            "round", "save", "since", "than", "through", "to", "toward", "towards", "under",
+            "underneath", "unlike", "until", "up", "upon", "versus", "via", "with", "within", "without"]
+
+        def previous_word(sentence="", base=""):
+            if base == "" or sentence == "": return ""
+            parts = sentence.split(base)
+            return parts[0].strip().split(" ")[-1] if len(parts) else None
+
+        def is_object(match, query, token):
+            previous = previous_word(query, token)
+            return is_preposition(previous) or previous.isdigit() or token.isnumeric() or token == query
+
+        predicates      = []
+        subjects        = []
+        objects         = []
+        propositions    = []
+        to_search       = set()
+        # Picks candidates for subjects and predicates
+        for idx, match in enumerate(matches):
+            subjects     += match["models"]
+            predicates   += match["relationships"] + match["literals"]
+            token         = match["token"]
+            # True when the current token is the last of the series
+            is_last_token = query.endswith(token)
+            # Objects are detected when they start and end by double quotes
+            if token.startswith('"') and token.endswith('"'):
+                # Remove the quote from the token
+                token = token.replace('"', '')
+                # We may search this term
+                to_search.add(token)
+            # Or if the previous word is a preposition
+            elif is_object(match, query, token):
+                if token not in to_search and len(token) > 2:
+                    # We may search this term
+                    to_search.add(token)
+
+        # Search all terms at once
+        objects += self.by_name(to_search)
+        # Only keep predicates that concern our subjects
+        subject_names = set([subject['name'] for subject in subjects])
+        predicates = filter(lambda predicate: predicate['subject'] in subject_names, predicates)
+
+        # We find some subjects
+        if len(subjects) and not len(predicates):
+            terms  = self.get_syntax().get("predicate").get("relationship")
+            terms += self.get_syntax().get("predicate").get("literal")
+            for subject in subjects:
+                # Gets all available terms for these subjects
+                predicates += [ term for term in terms if term["subject"] == subject["name"] ]
+
+        # Add a default and irrelevant object
+        if not len(objects): objects = [""]
+
+        # Generate proposition using RDF's parts
+        for subject in remove_duplicates(subjects):
+            for predicate in remove_duplicates(predicates):
+                for obj in remove_duplicates(objects):
+                    pred_sub = predicate.get("subject", None)
+                    # If the predicate has a subject
+                    # and it matches to the current one
+                    if pred_sub != None and pred_sub == subject.get("name", None):
+
+                        # Target Model of the predicate
+                        target = SearchTerm(
+                            subject=pred_sub,
+                            name=predicate["name"],
+                            topic=self.topic
+                        ).target
+
+                        if type(obj) is dict:
+                            obj_disp = obj["name"] or obj["label"]
+                            # Pass this predicate if this object doesn't match
+                            # with the current predicate's target
+                            if target != obj["model"]: continue
+                        else:
+                            obj_disp = obj
+                        # Value to inset into the proposition's label
+                        values = (subject["label"], predicate["label"], obj_disp,)
+                        # Build the label
+                        label = '%s that %s %s' % values
+                        propositions.append({
+                            'label'    : label,
+                            'subject'  : subject,
+                            'predicate': predicate,
+                            'object'   : obj
+                        })
+
+        # It might be a classic search
+        for obj in [ obj for obj in remove_duplicates(objects) if 'id' in obj ]:
+            # Build the label
+            label = obj.get("name", None)
+            propositions.append({
+                'label': label,
+                'subject': {
+                    "name": obj.get("id", None),
+                    "label": label
+                },
+                'predicate': {
+                    "label": "is instance of",
+                    "name": "<<INSTANCE>>"
+                },
+                'object': obj.get("model", None)
+            })
+        # Remove duplicates proposition dicts
+        return propositions
+
+    def get_syntax(self):
+        if not hasattr(self, "syntax"):
+            syntax = self.topic.get_syntax()
+            self.syntax = syntax
+        return self.syntax
--- app/detective/topics/common/summary.py
+++ app/detective/topics/common/summary.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from .errors                import ForbiddenError, UnauthorizedError
-from app.detective.models   import Topic, SearchTerm
+from app.detective.models   import Topic
+from app.detective.search   import Search
 from app.detective.neomatch import Neomatch
-from app.detective          import graph, search
-from difflib                import SequenceMatcher
+from app.detective          import graph
 from django.core.paginator  import Paginator, InvalidPage
 from django.http            import Http404, HttpResponse
 from neo4django.db          import connection
@@ -52,6 +52,8 @@ class SummaryResource(Resource):
         if hasattr(self, "syntax"): delattr(self, "syntax")
         # Get the current topic
         self.topic = self.get_topic_or_404(request=request)
+        # Create a search instance
+        self.search = Search(topic=self.topic)
         # Check for an optional method to do further dehydration.
         method = getattr(self, "summary_%s" % kwargs["pk"], None)
         if method:
@@ -278,7 +280,7 @@ class SummaryResource(Resource):
         limit     = int(request.GET.get('limit', 20))
         offset    = int(request.GET.get('offset', 0))
         query     = bundle.request.GET["q"].lower()
-        results   = self.search(query)
+        results   = self.search.by_name(query)
         paginator = Paginator(results, limit)
 
         try:
@@ -325,9 +327,9 @@ class SummaryResource(Resource):
         query        = request.GET["q"]
         query        = query.strip()
         # Find the kown match for the given query
-        matches      = self.find_matches(query)
+        matches      = self.search.find_matches(query)
         # Build and returns a list of proposal
-        propositions = self.build_propositions(matches, query)
+        propositions = self.search.build_propositions(matches, query)
 
         # Build paginator
         limit        = int(request.GET.get('limit', 20))
@@ -423,191 +425,6 @@ class SummaryResource(Resource):
         self.log_throttled_access(request)
         return response
 
-    def summary_syntax(self, bundle, request): return self.get_syntax(bundle, request)
-
-    def search(self, terms):
-        return search.by_name(terms, self.topic.app_label())
-
-    def ngrams(self, input):
-        input = input.split(' ')
-        output = []
-        end = len(input)
-        for n in range(1, end+1):
-            for i in range(len(input)-n+1):
-                output.append(input[i:i+n])
-        return output
-
-    def get_close_labels(self, token, lst, ratio=0.6):
-        """
-            Look for the given token into the list using labels
-        """
-        matches = []
-        for item in lst:
-            cpr = item["label"]
-            relevance = SequenceMatcher(None, token, cpr).ratio()
-            if relevance >= ratio:
-                item["relevance"] = relevance
-                matches.append(item)
-        return matches
-
-    def find_matches(self, query):
-        # Group ngram by following string
-        ngrams  = [' '.join(x) for x in self.ngrams(query) ]
-        matches = []
-        models  = self.get_syntax().get("subject").get("model")
-        rels    = self.get_syntax().get("predicate").get("relationship")
-        lits    = self.get_syntax().get("predicate").get("literal")
-        # Known models lookup for each ngram
-        for token in ngrams:
-            obj = {
-                'models'       : self.get_close_labels(token, models),
-                'relationships': self.get_close_labels(token, rels),
-                'literals'     : self.get_close_labels(token, lits),
-                'token'        : token
-            }
-            matches.append(obj)
-        return matches
-
-    def build_propositions(self, matches, query):
-        """
-            For now, a proposition follow the form
-            <subject> <predicat> <object>
-            Where a <subject>, is an "Named entity" or a Model
-            a <predicat> is a relationship type
-            and an <object> is a "Named entity" or a Model.
-            Later, as follow RDF standard, an <object> could be any data.
-        """
-        def remove_duplicates(lst):
-            seen = set()
-            new_list = []
-            for item in lst:
-                if type(item) is dict:
-                    # Create a hash of the dictionary
-                    obj = hash(frozenset(item.items()))
-                else:
-                    obj = hash(item)
-                if obj not in seen:
-                    seen.add(obj)
-                    new_list.append(item)
-            return new_list
-
-        def is_preposition(token=""):
-            return unicode(token).lower() in ["aboard", "about", "above", "across", "after", "against",
-            "along", "amid", "among", "anti", "around", "as", "at", "before", "behind", "below",
-            "beneath", "beside", "besides", "between", "beyond", "but", "by", "concerning",
-            "considering",  "despite", "down", "during", "except", "excepting", "excluding",
-            "following", "for", "from", "in", "inside", "into", "like", "minus", "near", "of",
-            "off", "on", "onto", "opposite", "outside", "over", "past", "per", "plus", "regarding",
-            "round", "save", "since", "than", "through", "to", "toward", "towards", "under",
-            "underneath", "unlike", "until", "up", "upon", "versus", "via", "with", "within", "without"]
-
-        def previous_word(sentence="", base=""):
-            if base == "" or sentence == "": return ""
-            parts = sentence.split(base)
-            return parts[0].strip().split(" ")[-1] if len(parts) else None
-
-        def is_object(match, query, token):
-            previous = previous_word(query, token)
-            return is_preposition(previous) or previous.isdigit() or token.isnumeric() or token == query
-
-        predicates      = []
-        subjects        = []
-        objects         = []
-        propositions    = []
-        to_search       = set()
-        # Picks candidates for subjects and predicates
-        for idx, match in enumerate(matches):
-            subjects     += match["models"]
-            predicates   += match["relationships"] + match["literals"]
-            token         = match["token"]
-            # True when the current token is the last of the series
-            is_last_token = query.endswith(token)
-            # Objects are detected when they start and end by double quotes
-            if token.startswith('"') and token.endswith('"'):
-                # Remove the quote from the token
-                token = token.replace('"', '')
-                # We may search this term
-                to_search.add(token)
-            # Or if the previous word is a preposition
-            elif is_object(match, query, token):
-                if token not in to_search and len(token) > 2:
-                    # We may search this term
-                    to_search.add(token)
-
-        # Search all terms at once
-        objects += self.search(to_search)
-        # Only keep predicates that concern our subjects
-        subject_names = set([subject['name'] for subject in subjects])
-        predicates = filter(lambda predicate: predicate['subject'] in subject_names, predicates)
-
-        # We find some subjects
-        if len(subjects) and not len(predicates):
-            terms  = self.get_syntax().get("predicate").get("relationship")
-            terms += self.get_syntax().get("predicate").get("literal")
-            for subject in subjects:
-                # Gets all available terms for these subjects
-                predicates += [ term for term in terms if term["subject"] == subject["name"] ]
-
-        # Add a default and irrelevant object
-        if not len(objects): objects = [""]
-
-        # Generate proposition using RDF's parts
-        for subject in remove_duplicates(subjects):
-            for predicate in remove_duplicates(predicates):
-                for obj in remove_duplicates(objects):
-                    pred_sub = predicate.get("subject", None)
-                    # If the predicate has a subject
-                    # and it matches to the current one
-                    if pred_sub != None and pred_sub == subject.get("name", None):
-
-                        # Target Model of the predicate
-                        target = SearchTerm(
-                            subject=pred_sub,
-                            name=predicate["name"],
-                            topic=self.topic
-                        ).target
-
-                        if type(obj) is dict:
-                            obj_disp = obj["name"] or obj["label"]
-                            # Pass this predicate if this object doesn't match
-                            # with the current predicate's target
-                            if target != obj["model"]: continue
-                        else:
-                            obj_disp = obj
-                        # Value to inset into the proposition's label
-                        values = (subject["label"], predicate["label"], obj_disp,)
-                        # Build the label
-                        label = '%s that %s %s' % values
-                        propositions.append({
-                            'label'    : label,
-                            'subject'  : subject,
-                            'predicate': predicate,
-                            'object'   : obj
-                        })
-
-        # It might be a classic search
-        for obj in [ obj for obj in remove_duplicates(objects) if 'id' in obj ]:
-            # Build the label
-            label = obj.get("name", None)
-            propositions.append({
-                'label': label,
-                'subject': {
-                    "name": obj.get("id", None),
-                    "label": label
-                },
-                'predicate': {
-                    "label": "is instance of",
-                    "name": "<<INSTANCE>>"
-                },
-                'object': obj.get("model", None)
-            })
-        # Remove duplicates proposition dicts
-        return propositions
-
-    def get_syntax(self, bundle=None, request=None):
-        if not hasattr(self, "syntax"):
-            syntax = self.topic.get_syntax()
-            self.syntax = syntax
-        return self.syntax
+    def summary_syntax(self, bundle, request): return self.search.get_syntax()
 
 # EOF
